1abf68f25b0b6564168ccfb0d5bb15119069c16a
iliaShamakhia <shamakhiailia@gmail.com>
2025-11-29 16:23:28 +0400

    fix: fix lint errors

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,11 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Param,
-  Delete,
-  HttpCode
-} from '@nestjs/common';
+import { Controller, Get, Post, Param, Delete, HttpCode } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 
 @Controller('favs')
