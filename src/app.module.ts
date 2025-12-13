e6e135fe6a1d4df4739dd6872edf45fba5ff1d98
iliaShamakhia <shamakhiailia@gmail.com>
2025-12-13 19:27:11 +0400

    init: initial add logging module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ArtistsModule } from './artists/artists.module';
 import { TracksModule } from './tracks/tracks.module';
 import { AlbumsModule } from './albums/albums.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { LoggingModule } from './logging/logging.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { FavoritesModule } from './favorites/favorites.module';
     TracksModule,
     AlbumsModule,
     FavoritesModule,
+    LoggingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,18 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import * as YAML from 'yamljs';
 import * as dotenv from 'dotenv';
+import { MyLoggerService } from './logging/logging.service';
 
 dotenv.config();
 
 const port = process.env.PORT || 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  app.useLogger(app.get(MyLoggerService));
 
   const document = YAML.load('doc/api.yaml');
 
